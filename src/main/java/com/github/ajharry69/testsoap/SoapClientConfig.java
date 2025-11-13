6e287b90e4832f758175f0a475c8ac0384ab3137
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-11-14 00:03:30 +0300

    Refactor SOAP client configuration to use `SaajSoapMessageFactory` and updated `HttpComponents5MessageSender`

--- src/main/java/com/github/ajharry69/testsoap/SoapClientConfig.java
+++ src/main/java/com/github/ajharry69/testsoap/SoapClientConfig.java
@@ -1,14 +1,15 @@
 package com.github.ajharry69.testsoap;
 
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.core5.util.Timeout;
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.SOAPException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.transport.http.SimpleHttpComponents5MessageSender;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+import org.springframework.ws.transport.http.HttpComponents5MessageSender;
+
+import java.time.Duration;
 
 @Configuration
 class SoapClientConfig {
@@ -17,14 +18,28 @@ class SoapClientConfig {
         var marshaller = new Jaxb2Marshaller();
         // Scan ALL packages that contain JAXB models
         marshaller.setPackagesToScan(
-                "com.github.ajharry69.testsoap"
+                "com.github.ajharry69.testsoap.countries",
+                "com.github.ajharry69.testsoap.temperature"
         );
         return marshaller;
     }
 
     @Bean
-    public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller, SimpleHttpComponents5MessageSender messageSender) {
-        var template = new WebServiceTemplate();
+    public SaajSoapMessageFactory messageFactory() {
+        try {
+            SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory();
+            messageFactory.setMessageFactory(MessageFactory.newInstance());
+            messageFactory.afterPropertiesSet();
+//            messageFactory.setSoapVersion(SoapVersion.SOAP_12);
+            return messageFactory;
+        } catch (SOAPException e) {
+            throw new RuntimeException("Failed to create SOAP MessageFactory", e);
+        }
+    }
+
+    @Bean
+    public WebServiceTemplate webServiceTemplate(SaajSoapMessageFactory messageFactory, Jaxb2Marshaller marshaller, HttpComponents5MessageSender messageSender) {
+        var template = new WebServiceTemplate(messageFactory);
         template.setMarshaller(marshaller);
         template.setUnmarshaller(marshaller);
 
@@ -34,24 +49,15 @@ class SoapClientConfig {
     }
 
     @Bean
-    public SimpleHttpComponents5MessageSender httpComponentsMessageSender() {
-        // 1. Configure Connection Pooling (CRITICAL for high traffic)
-        var pool = new PoolingHttpClientConnectionManager();
-        pool.setMaxTotal(200); // Max total connections in the pool
-        pool.setDefaultMaxPerRoute(50); // Max concurrent connections to a single host (e.g., domain.com)
-
-        // 2. Configure Timeouts (NEVER go to production without these)
-        var config = RequestConfig.custom()
-                .setConnectTimeout(Timeout.ofSeconds(5))
-                .setResponseTimeout(Timeout.ofSeconds(10))
-                .build();
-
-        // 3. Build the client
-        var client = HttpClients.custom()
-                .setConnectionManager(pool)
-                .setDefaultRequestConfig(config)
-                .build();
-
-        return new SimpleHttpComponents5MessageSender(client);
+    public HttpComponents5MessageSender httpComponentsMessageSender() {
+        var sender = new HttpComponents5MessageSender();
+        sender.setConnectionTimeout(Duration.ofSeconds(5));
+        sender.setReadTimeout(Duration.ofSeconds(10));
+        sender.setMaxTotalConnections(200);
+        /*sender.setMaxConnectionsPerHost(
+                Map.of("www.w3schools.com", "50")
+        );*/
+        sender.setAcceptGzipEncoding(false);
+        return sender;
     }
 }
