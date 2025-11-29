e850d3a66a341c11972cad4a5c4a70459e60b3ff
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-11-29 22:16:02 +0300

    Fix failing tests

--- src/test/java/com/github/ajharry69/testsoap/countries/CountriesControllerTest.java
+++ src/test/java/com/github/ajharry69/testsoap/countries/CountriesControllerTest.java
@@ -3,10 +3,13 @@ package com.github.ajharry69.testsoap.countries;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -33,11 +36,27 @@ class CountriesControllerTest {
         b.setName("Afghanistan");
         response.setCountries(List.of(a, b));
 
-        when(service.get()).thenReturn(response);
+        when(service.get())
+                .thenReturn(response);
 
-        mvc.perform(get("/countries"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.countries[0].isoCode").value("AX"))
-                .andExpect(jsonPath("$.countries[1].name").value("Afghanistan"));
+        var headers = new HttpHeaders(
+                MultiValueMap.fromSingleValue(
+                        Map.of(
+                                "X-FeatureName", "101",
+                                "X-ServiceCode", "101",
+                                "X-ServiceName", "101",
+                                "X-MinorServiceVersion", "101",
+                                "X-ChannelCategory", "101",
+                                "X-ChannelCode", "101",
+                                "X-ChannelName", "101"
+                        )
+                )
+        );
+        mvc.perform(get("/countries").headers(headers))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.countries[0].isoCode").value("AX"),
+                        jsonPath("$.countries[1].name").value("Afghanistan")
+                );
     }
 }
--- src/test/java/com/github/ajharry69/testsoap/temperature/TemperatureControllerTest.java
+++ src/test/java/com/github/ajharry69/testsoap/temperature/TemperatureControllerTest.java
@@ -3,10 +3,13 @@ package com.github.ajharry69.testsoap.temperature;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -27,14 +30,29 @@ class TemperatureControllerTest {
 
     @Test
     void get_returns_200_and_list_payload() throws Exception {
-        TemperatureResponse r = new TemperatureResponse();
-        r.setDegreesCelsius("98.98");
+        var response = new TemperatureResponse();
+        response.setDegreesCelsius("98.98");
         when(service.get(any()))
-                .thenReturn(List.of(r));
+                .thenReturn(List.of(response));
 
-        mvc.perform(get("/temperature?fahrenheitReading=100"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].degreesCelsius", is("98.98")));
+        var headers = new HttpHeaders(
+                MultiValueMap.fromSingleValue(
+                        Map.of(
+                                "X-FeatureName", "101",
+                                "X-ServiceCode", "101",
+                                "X-ServiceName", "101",
+                                "X-MinorServiceVersion", "101",
+                                "X-ChannelCategory", "101",
+                                "X-ChannelCode", "101",
+                                "X-ChannelName", "101"
+                        )
+                )
+        );
+        mvc.perform(get("/temperature?fahrenheitReading=100").headers(headers))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$", hasSize(1)),
+                        jsonPath("$[0].degreesCelsius", is("98.98"))
+                );
     }
 }
